121b02d976ce17babc0f346a8498bcaf84bdd9d5
Joni <joni.hell@outlook.com>
2022-04-17 21:35:31 +0300

    Revert "namespace changes"
    This reverts commit b24d56ff5445e5f7bb0295080ea436db5fdd9f3b.

--- src/Timer.php
+++ src/Timer.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Jonett\Timer;
+namespace SebastianBergmann\Timer;
 
 use function array_pop;
 use function hrtime;
